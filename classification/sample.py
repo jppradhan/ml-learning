e598cd1977c2e50cb6715ff278128a3983b29999
Jyoti Prakash <jyotiprakash.pradhan@vimeo.com>
2020-01-07 11:44:48 +0530

    more samples

--- classification/sample.py
+++ classification/sample.py
@@ -19,3 +19,30 @@ model_2 = LogisticRegression()
 model_2.fit(exam_features_scaled_train, passed_exam_2_train)
 # Predict whether the students will pass here
 passed_predictions_2 = model_2.predict(exam_features_scaled_test)
+
+#Breast cancer model learing
+import codecademylib3_seaborn
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+cancer = load_breast_cancer()
+X_train,X_test,Y_train,Y_test = train_test_split(cancer.data,cancer.target, train_size=.75, random_state=0)
+
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+
+predictions = model.predict(X_test)
+score = model.score(X_test, Y_test)
+#print(score)
+
+#print(classification_report(Y_test, predictions))
+print(X_test[1])
+predict=model.predict([X_test[1]])
+preds = cancer.target_names[predict]  # mapping the output label with the meaning of label.
+print(preds)
